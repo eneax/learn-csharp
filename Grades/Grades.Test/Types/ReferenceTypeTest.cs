6b6b351ac8401b3be8425c0a949687f96751af97
eneax <19818937+eneax@users.noreply.github.com>
2018-10-10 16:45:05 +0200

    Reference Types

---
+++ Grades/Grades.Test/Types/ReferenceTypeTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades.Test.Types
+{
+    [TestClass]
+    public class ReferenceTypeTest
+    {
+        [TestMethod]
+        public void VariablesHoldAReference()
+        {
+            GradeBook g1 = new GradeBook();
+            GradeBook g2 = g1;
+
+            g1.Name = "Scott's grade book";
+            Assert.AreEqual(g1.Name, g2.Name);
+        }
+    }
+}
+
+/*
+Reference Types
+- Variables store a reference to an object 
+    * every time I create a class, I'm creating a new reference type
+    * multiple variables can point to the same object
+*/
