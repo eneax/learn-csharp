ec3d54d32724da0549a1b5e6a3b225109bbb7b92
eneax <19818937+eneax@users.noreply.github.com>
2018-10-10 16:56:27 +0200

    Value Types

--- Grades/Grades.Test/Types/ReferenceTypeTest.cs
+++ Grades/Grades.Test/Types/ReferenceTypeTest.cs
@@ -11,13 +11,24 @@ namespace Grades.Test.Types
     public class ReferenceTypeTest
     {
         [TestMethod]
-        public void VariablesHoldAReference()
+        public void IntVariablesHoldAValue()
+        {
+            int x1 = 100;
+            int x2 = x1;
+
+            x1 = 4;
+            Assert.AreNotEqual(x1, x2);
+        }
+
+        [TestMethod]
+        public void GradeBookVariablesHoldAReference()
         {
             GradeBook g1 = new GradeBook();
             GradeBook g2 = g1;
 
+            g1 = new GradeBook();
             g1.Name = "Scott's grade book";
-            Assert.AreEqual(g1.Name, g2.Name);
+            Assert.AreNotEqual(g1.Name, g2.Name);
         }
     }
 }
@@ -27,4 +38,15 @@ Reference Types
 - Variables store a reference to an object 
     * every time I create a class, I'm creating a new reference type
     * multiple variables can point to the same object
+*/
+
+
+/*
+Value Types
+- Variables hold the value
+    * No pointers, no references
+- They are generally faster than Reference Types to allocate in memory
+- They are smaller in size
+- Many build-in primitives are value types
+    * int, double, float
 */
