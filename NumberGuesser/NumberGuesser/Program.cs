efa664664cc7aeecad31bd0c8c906da43479e95f
eneax <19818937+eneax@users.noreply.github.com>
2018-10-13 21:48:55 +0200

    Set app variables

--- NumberGuesser/NumberGuesser/Program.cs
+++ NumberGuesser/NumberGuesser/Program.cs
@@ -10,6 +10,18 @@ namespace NumberGuesser
     {
         static void Main(string[] args)
         {
+            // Set app variables
+            string appName = "Number Guesser";
+            string appVersion = "1.0.0";
+            string appAuthor = "eneax";
+
+            Console.WriteLine($"{appName}: Version {appVersion} by {appAuthor}");
         }
     }
 }
+
+/*
+- We use classes to create objects
+- When we use static, we refer to the function itself
+- Main --> Entry Point Method (a method is like a function inside a class)
+*/
