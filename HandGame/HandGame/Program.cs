cc7a0764a62bb5feb7063e9d856966318847b145
eneax <19818937+eneax@users.noreply.github.com>
2018-10-15 09:34:30 +0200

    Setup rules

--- HandGame/HandGame/Program.cs
+++ HandGame/HandGame/Program.cs
@@ -13,10 +13,13 @@ namespace HandGame
             // Define variables
             string inputPlayer, inputCPU;
             int randomInt;
+            int scorePlayer = 0;
+            int scoreCPU = 0;
 
             // Ask user to choose an option
             Console.Write("Choose between ROCK, PAPER AND SCISSORS:     ");
             inputPlayer = Console.ReadLine();
+            inputPlayer = inputPlayer.ToUpper();
 
             // Generate random num from 1 to 3
             Random rnd = new Random();
@@ -26,12 +29,57 @@ namespace HandGame
             {
                 case 1:
                     inputCPU = "ROCK";
+                    Console.WriteLine("Computer chose ROCK");
+                    if (inputPlayer == "ROCK")
+                    {
+                        Console.WriteLine("DRAW");
+                    }
+                    else if (inputPlayer == "PAPER")
+                    {
+                        Console.WriteLine("PLAYER WINS");
+                        scorePlayer++;
+                    }
+                    else if (inputPlayer == "SCISSORS")
+                    {
+                        Console.WriteLine("CPU WINS");
+                        scoreCPU++;
+                    }
                     break;
                 case 2:
                     inputCPU = "PAPER";
+                    Console.WriteLine("Computer chose PAPER");
+                    if (inputPlayer == "ROCK")
+                    {
+                        Console.WriteLine("CPU WINS");
+                        scoreCPU++;
+                    }
+                    else if (inputPlayer == "PAPER")
+                    {
+                        Console.WriteLine("DRAW");
+                    }
+                    else if (inputPlayer == "SCISSORS")
+                    {
+                        Console.WriteLine("PLAYER WINS");
+                        scorePlayer++;
+                    }
                     break;
                 case 3:
                     inputCPU = "SCISSORS";
+                    Console.WriteLine("Computer chose SCISSORS");
+                    if (inputPlayer == "ROCK")
+                    {
+                        Console.WriteLine("PLAYER WINS");
+                        scorePlayer++;
+                    }
+                    else if (inputPlayer == "PAPER")
+                    {
+                        Console.WriteLine("CPU WINS");
+                        scoreCPU++;
+                    }
+                    else if (inputPlayer == "SCISSORS")
+                    {
+                        Console.WriteLine("DRAW");
+                    }
                     break;
                 default:
                     break;
