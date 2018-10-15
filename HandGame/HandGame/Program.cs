f994777e55f374dd8955337c42ba26b057a57837
eneax <19818937+eneax@users.noreply.github.com>
2018-10-15 09:20:12 +0200

    Generate random num

--- HandGame/HandGame/Program.cs
+++ HandGame/HandGame/Program.cs
@@ -17,6 +17,25 @@ namespace HandGame
             // Ask user to choose an option
             Console.Write("Choose between ROCK, PAPER AND SCISSORS:     ");
             inputPlayer = Console.ReadLine();
+
+            // Generate random num from 1 to 3
+            Random rnd = new Random();
+            randomInt = rnd.Next(1, 4);
+
+            switch (randomInt)
+            {
+                case 1:
+                    inputCPU = "ROCK";
+                    break;
+                case 2:
+                    inputCPU = "PAPER";
+                    break;
+                case 3:
+                    inputCPU = "SCISSORS";
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
