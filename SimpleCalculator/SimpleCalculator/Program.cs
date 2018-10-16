21bc48047745b3a50f6ea94dcdcc8fb6886be640
eneax <19818937+eneax@users.noreply.github.com>
2018-10-16 10:35:11 +0200

    Setup calculator

--- SimpleCalculator/SimpleCalculator/Program.cs
+++ SimpleCalculator/SimpleCalculator/Program.cs
@@ -10,6 +10,57 @@ namespace SimpleCalculator
     {
         static void Main(string[] args)
         {
+            // Define app variables
+            double firstNum;
+            double secondNum;
+            double answer;
+            string ops;
+
+            // Display app info
+            Console.WriteLine("Console Calculator");
+            Console.WriteLine("*******************");
+
+            // Prompt user for first number
+            Console.WriteLine("Enter first number: ");
+            firstNum = Double.Parse(Console.ReadLine());
+
+            // Prompt user for math operator
+            Console.WriteLine("Select an operator [+, -, *, /, %, ^]: ");
+            ops = Console.ReadLine();
+
+            // Prompt user for second number
+            Console.WriteLine("Enter second number: ");
+            secondNum = Double.Parse(Console.ReadLine());
+
+            // Set possible answers
+            switch (ops)
+            {
+                case "+":
+                    answer = firstNum + secondNum;
+                    break;
+                case "-":
+                    answer = firstNum - secondNum;
+                    break;
+                case "*":
+                    answer = firstNum * secondNum;
+                    break;
+                case "/":
+                    answer = firstNum / secondNum;
+                    break;
+                case "%":
+                    answer = firstNum % secondNum;
+                    break;
+                case "^":
+                    answer = Math.Pow(firstNum, secondNum);
+                    break;
+                default:
+                    answer = 0;
+                    break;
+            }
+
+            // Print final result
+            Console.WriteLine($"{firstNum} {ops} {secondNum} = {answer}");
+            Console.ReadKey();
         }
     }
 }
