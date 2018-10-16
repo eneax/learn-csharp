4c673fb68242acc3d80f00221a6492332b9912be
eneax <19818937+eneax@users.noreply.github.com>
2018-10-16 11:51:33 +0200

    Refactor code

--- SimpleCalculator/SimpleCalculator/Program.cs
+++ SimpleCalculator/SimpleCalculator/Program.cs
@@ -13,54 +13,82 @@ namespace SimpleCalculator
             // Define app variables
             double firstNum;
             double secondNum;
-            double answer;
+            double input;
             string ops;
 
             // Display app info
             Console.WriteLine("Console Calculator");
             Console.WriteLine("*******************");
 
-            // Prompt user for first number
-            Console.WriteLine("Enter first number: ");
-            firstNum = Double.Parse(Console.ReadLine());
-
-            // Prompt user for math operator
-            Console.WriteLine("Select an operator [+, -, *, /, %, ^]: ");
-            ops = Console.ReadLine();
-
-            // Prompt user for second number
-            Console.WriteLine("Enter second number: ");
-            secondNum = Double.Parse(Console.ReadLine());
-
-            // Set possible answers
-            switch (ops)
+            while (true)
             {
-                case "+":
-                    answer = firstNum + secondNum;
-                    break;
-                case "-":
-                    answer = firstNum - secondNum;
-                    break;
-                case "*":
-                    answer = firstNum * secondNum;
-                    break;
-                case "/":
-                    answer = firstNum / secondNum;
-                    break;
-                case "%":
-                    answer = firstNum % secondNum;
-                    break;
-                case "^":
-                    answer = Math.Pow(firstNum, secondNum);
-                    break;
-                default:
-                    answer = 0;
-                    break;
-            }
+                // Prompt user for first number
+                Console.Write("\nEnter first number:    ");
+                firstNum = Double.Parse(Console.ReadLine());
 
-            // Print final result
-            Console.WriteLine($"{firstNum} {ops} {secondNum} = {answer}");
-            Console.ReadKey();
+                // Prompt user for math operator
+                Console.Write("Select an operator [+, -, *, /, %, ^]:   ");
+                ops = Console.ReadLine();
+
+                // Prompt user for second number
+                Console.Write("Enter second number:     ");
+                secondNum = Double.Parse(Console.ReadLine());
+
+                // Set possible answers
+                switch (ops)
+                {
+                    case "+":
+                        input = firstNum + secondNum;
+                        break;
+                    case "-":
+                        input = firstNum - secondNum;
+                        break;
+                    case "*":
+                        input = firstNum * secondNum;
+                        break;
+                    case "/":
+                        input = firstNum / secondNum;
+                        break;
+                    case "%":
+                        input = firstNum % secondNum;
+                        break;
+                    case "^":
+                        input = Math.Pow(firstNum, secondNum);
+                        break;
+                    default:
+                        input = 0;
+                        break;
+                }
+
+                // Print final result
+                Console.WriteLine($"\nFinal result: {firstNum} {ops} {secondNum} = {input}");
+
+
+                // Keep asking to run calc again until user answers [y or n]
+                while (true)
+                {
+                    // Ask user to run again
+                    Console.WriteLine("\n\nRun again? [y or n]");
+
+                    // Get answer
+                    string answer = Console.ReadLine().ToLower();
+
+                    if (answer == "y")
+                    {
+                        break;
+                    }
+                    else if (answer == "n")
+                    {
+                        Console.WriteLine("\nBye Bye!");
+                        return;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid entry!");
+                        continue;
+                    }
+                }
+            }
         }
     }
 }
