b822cd84d3fd10a1152c832cfc721c070da59cbc
eneax <19818937+eneax@users.noreply.github.com>
2018-11-05 21:18:49 +0100

    Add exercise one for Arrays and Lists

--- CSharpFundamentals/Lists/Program.cs
+++ CSharpFundamentals/Lists/Program.cs
@@ -60,6 +60,65 @@ namespace Lists
             // Clear() --> removes all items from the List
             numbers.Clear();
             Console.WriteLine("Count of numbers: " + numbers.Count());
+
+
+
+            // Exercises
+            var exercises = new Exercises();
+            exercises.Number1();
+        }
+    }
+
+
+
+    public class Exercises
+    {
+        /*
+          Ex. 1
+              When you post a message on Facebook, depending on the number of people who like your post, 
+              Facebook displays different information.
+
+              - If no one likes your post, it doesn't display anything.
+              - If only one person likes your post, it displays: [Friend's Name] likes your post.
+              - If two people like your post, it displays: [Friend 1] and [Friend 2] like your post.
+              - If more than two people like your post, it displays: [Friend 1], [Friend 2] and [Number of Other People] others like your post.
+
+              Write a program and continuously ask the user to enter different names, 
+              until the user presses Enter (without supplying a name). 
+              Depending on the number of names provided, display a message based on the above pattern. 
+        */
+        public void Number1()
+        {
+            var names = new List<string>();
+
+            while (true)
+            {
+                Console.Write("Type a name (or hit ENTER to quit): ");
+
+                var input = Console.ReadLine();
+                if (string.IsNullOrEmpty(input))
+                {
+                    break;
+                }
+                names.Add(input);
+            }
+
+            if (names.Count > 2)
+            {
+                Console.WriteLine($"{names[0]}, {names[1]} and {names.Count - 2} liked your post!");
+            }
+            else if (names.Count == 2)
+            {
+                Console.WriteLine($"{names[0]} and {names[1]} liked your post!");
+            }
+            else if (names.Count == 1)
+            {
+                Console.WriteLine($"{names[0]} liked your post!");
+            }
+            else
+            {
+                Console.WriteLine();
+            }
         }
     }
 }
