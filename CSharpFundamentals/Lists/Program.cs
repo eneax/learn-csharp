b7d7cc9c7c12a7d2214caf27a7d9318759d74773
eneax <19818937+eneax@users.noreply.github.com>
2018-11-06 09:57:02 +0100

    Add exercise three for Arrays and Lists

--- CSharpFundamentals/Lists/Program.cs
+++ CSharpFundamentals/Lists/Program.cs
@@ -67,6 +67,7 @@ namespace Lists
             var exercises = new Exercises();
             exercises.Number1();
             exercises.Number2();
+            exercises.Number3();
         }
     }
 
@@ -143,6 +144,40 @@ namespace Lists
 
             var reversed = new string(charArray);
             Console.WriteLine("Reversed name: " + reversed);
+        }
+
+
+
+        /*
+        Ex. 3
+        Write a program and ask the user to enter 5 numbers.
+        If a number has been previously entered, display an error message and ask the user to re-try. 
+        Once the user successfully enters 5 unique numbers, sort them and display the result on the console.
+        */
+        public void Number3()
+        {
+            var numbers = new List<int>();
+
+            while (numbers.Count < 5)
+            {
+                Console.Write("Enter a number: ");
+                var number = Convert.ToInt32(Console.ReadLine());
+
+                if (numbers.Contains(number))
+                {
+                    Console.WriteLine($"You've already entered {number}");
+                    continue;
+                }
+
+                numbers.Add(number);
+            }
+
+            numbers.Sort();
+
+            foreach (var n in numbers)
+            {
+                Console.WriteLine(n);
+            }
         }
     }
 }
