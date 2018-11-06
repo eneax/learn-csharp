a74f7f46711575deb1dc12bb2109fedd29ad80d4
eneax <19818937+eneax@users.noreply.github.com>
2018-11-06 18:07:42 +0100

    Add DateTime examples

---
+++ CSharpFundamentals/Dates/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Working with Dates
+            var dateTime = new DateTime(2018, 1, 1);
+
+
+            // Get current date 
+            var now = DateTime.Now;
+            var today = DateTime.Today;
+
+            Console.WriteLine("Hour: " + now.Hour);
+            Console.WriteLine("Minutes: " + now.Minute);
+
+
+            // DateTime is immutable --> to modify, we need to use Add
+            var tomorrow = now.AddDays(1);
+            var yesterday = now.AddDays(-1);
+
+
+            // Format DateTime to a string
+            Console.WriteLine(now.ToLongDateString());
+            Console.WriteLine(now.ToShortDateString());
+            Console.WriteLine(now.ToLongTimeString());
+            Console.WriteLine(now.ToShortTimeString());
+
+
+            // Custom Date and Time Format Strings
+            Console.WriteLine(now.ToString());
+            Console.WriteLine(now.ToString("yyyy-MM-dd HH:mm"));
+        }
+    }
+}
