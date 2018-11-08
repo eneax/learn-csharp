588f13a6caa2f4729db0dd4313bd5272f4ddc8ed
eneax <19818937+eneax@users.noreply.github.com>
2018-11-08 11:58:24 +0100

    Add PathClass examples

---
+++ CSharpFundamentals/PathClass/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PathClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"C:\Projects\CsharpFundamentals\HelloWorld\HelloWorld.sln";
+
+            // Get the extension at the end of the path '.sln'
+            Path.GetExtension(path);
+
+            // Get the file name
+            Path.GetFileName(path);
+            Path.GetFileNameWithoutExtension(path);
+
+            // Get the directory name
+            Path.GetDirectoryName(path);
+        }
+    }
+}
