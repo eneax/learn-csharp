12a30fc7f95849dfcd52fe6809436e2e32b0d4f9
eneax <19818937+eneax@users.noreply.github.com>
2018-11-08 11:30:00 +0100

    Intro to File and File Info

---
+++ CSharpFundamentals/07 - Working with Files.md
@@ -0,0 +1,25 @@
+# Working with Files
+
+All the classes that we need to work with files are located into the namespace 'System.IO'.
+
+
+## File and FileInfo Classes
+
+They provide methods for creating, copying, deleting, moving and opening of files.
+- FileInfo provides 'instance' methods
+- File provides 'static' methods
+
+When you use File, the operative system does some checking to verify that you have access to a file.
+So if you have a lot of operations, this process will affect the performance of your application.
+With FileInfo class, the security check is done only during the creation of the FileInfo object.
+
+
+## Directory and DirectoryInfo
+
+- DirectoryInfo provides 'instance' methods
+- Directory provides 'static' methods
+
+
+## Path Class
+
+It provides methods to work with a string that contains a file or a directory path information.
---
+++ CSharpFundamentals/FileandFileInfo/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileandFileInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            File
+                --> provides 'static methods' (no need to create an object)
+                --> useful for small operations
+            */
+            var path = @"c:\somefile.jpg";
+
+            /*
+            Copy() Method
+                - copy file from one directory ('c') to another ('d')
+                - 'true' --> if the file already exists, then overwrite it
+            */
+            File.Copy(@"c:\temp\myfile.jpeg", @"d:\temp\myfile.jpeg", true);
+
+
+            /*
+            Delete() Method
+                - provide a path to a file and the 'Delete()' method will delete it
+            */
+            File.Delete(path);
+
+
+            // Exists()
+            if (File.Exists(path))
+            {
+                // do something
+            }
+
+
+            // ReadAllText() --> takes a path and returns a string
+            var content = File.ReadAllText(path);
+
+
+
+            /*
+            FileInfo() 
+                --> provides 'instance methods' (you need to create an object first)
+                --> useful for big operations
+            */
+            var fileInfo = new FileInfo(path);
+
+
+            // CopyTo() and Delete()
+            fileInfo.CopyTo("...");
+            fileInfo.Delete();
+
+
+            // Exists --> it's not a methods, but a property
+            if (fileInfo.Exists)
+            {
+                // do something
+            }
+        }
+    }
+}
