b53e9ad7a8807b64eb345eb8916534851401854d
eneax <19818937+eneax@users.noreply.github.com>
2018-11-08 18:27:55 +0100

    Initial commit CSharpIntermediate

---
+++ CSharpIntermediate/Classes/Classes/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class Person
+    {
+        public string Name;
+
+        public void Introduce(string to)
+        {
+            Console.WriteLine($"Hello {to}, I'm {Name}!");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            person.Name = "John";
+            person.Introduce("Enea");
+        }
+    }
+}
