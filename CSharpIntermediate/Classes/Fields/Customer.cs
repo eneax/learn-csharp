8fa96730bfd754a5291f2ea7e294be85f915e72c
eneax <19818937+eneax@users.noreply.github.com>
2018-11-09 11:15:05 +0100

    Add Fields example

---
+++ CSharpIntermediate/Classes/Fields/Customer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fields
+{
+    public class Customer
+    {
+        public int ID;
+        public string Name;
+
+        // Create and initialize List<Order> field 
+        // readonly --> so it will be initialized only once
+        readonly public List<Order> Orders = new List<Order>();
+
+        public Customer(int id)
+        {
+            this.ID = id;
+        }
+
+        public Customer(int id, string name)
+            : this(id)
+        {
+            this.Name = name;
+        }
+
+        public void Promote()
+        {
+            // ...
+        }
+    }
+}
+
+/*
+A field is like a variable that we declare at a class level 
+and use that to store data about a class.
+*/
