8559136fcb82d025143c680610b390c719bb47d9
eneax <19818937+eneax@users.noreply.github.com>
2018-11-18 21:21:06 +0100

    Add Indexers

---
+++ CSharpIntermediate/Classes/Indexers/HttpCookie.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexers
+{
+    public class HttpCookie
+    {
+        // Dictionary uses a hash table to store values
+        private readonly Dictionary<string, string> _dictionary;
+
+        public DateTime Expiry { get; set; }
+
+        public HttpCookie()
+        {
+            _dictionary = new Dictionary<string, string>();
+        }
+
+        // We declare indexers just like properties
+        public string this[string key]
+        {
+            get { return _dictionary[key]; }
+            set { _dictionary[key] = value; }
+        }
+    }
+}
