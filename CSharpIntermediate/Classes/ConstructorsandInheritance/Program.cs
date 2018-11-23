f34f488daa49845e7dbf22e5c296dbc38ab946fb
eneax <19818937+eneax@users.noreply.github.com>
2018-11-23 11:19:57 +0100

    Add examples for Constructors and Inheritance

---
+++ CSharpIntermediate/Classes/ConstructorsandInheritance/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorsandInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var car = new Car("XYZ123");
+            // Vehicle is being initialized before Car
+        }
+    }
+}
