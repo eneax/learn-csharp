f1d7321ef09243697f4a52077234482092c2ab4a
eneax <19818937+eneax@users.noreply.github.com>
2018-11-24 10:37:07 +0100

    Add example for MethodOverriding

---
+++ CSharpIntermediate/Classes/MethodOverriding/Shape.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverriding
+{
+    public class Circle : Shape
+    {
+        public override void Draw()
+        {
+            // Any code specific to the circle itself
+            Console.WriteLine("Draw a Circle");
+        }
+    }
+
+    public class Rectangle : Shape
+    {
+        public override void Draw()
+        {
+            // Any code specific to the rectangle itself
+            Console.WriteLine("Draw a Rectangle");
+        }
+    }
+
+    public class Triangle : Shape
+    {
+        public override void Draw()
+        {
+            // Any code specific to the triangle itself
+            Console.WriteLine("Draw a Triangle");
+        }
+    }
+
+    public class Shape
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public Position Position { get; set; }
+
+        public virtual void Draw()
+        {
+
+        }
+    }
+}
