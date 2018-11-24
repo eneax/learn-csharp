145d30373f82652ce94bde995705f1fed65f11ea
eneax <19818937+eneax@users.noreply.github.com>
2018-11-24 21:25:52 +0100

    Add examples for Extensibility

---
+++ CSharpIntermediate/Classes/Extensibility/DBMigrator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensibility
+{
+    
+
+    public class DBMigrator
+    {
+        private readonly ILogger _logger;
+
+        public DBMigrator(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Migrate()
+        {
+            _logger.LogInfo($"Migration started at {DateTime.Now}");
+
+            // Details of migrating the database
+
+            _logger.LogInfo($"Migration finished at {DateTime.Now}");
+        }
+    }
+}
---
+++ CSharpIntermediate/Classes/Extensibility/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/*
+We can use interfaces to change our application’s behaviour by “extending” its code,
+rather than changing the existing code.
+*/
+namespace Extensibility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var dbMigrator = new DBMigrator(new ConsoleLogger());
+            var dbMigrator = new DBMigrator(new FileLogger("C:\\projects\boh.txt"));
+            dbMigrator.Migrate();
+        }
+    }
+}
